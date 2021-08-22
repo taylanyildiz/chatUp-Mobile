4d531e5e3d223bd2cbff097caaf6b0a629872f87
Taylan YILDIZ <taylanyildz@outlook.com>
2021-08-22 06:20:02 +0300

    Update restful_constant.dart

--- lib/constants/restful_constant.dart
+++ lib/constants/restful_constant.dart
@@ -3,9 +3,9 @@ class RestfulConstant {
 
   static const String API_VERSION = '/api/v1';
 
-  static const String REMOTE_HOST = 'http://213.238.180.11';
-  static const String LOCAL_HOST = 'http://localhost';
-  static const String PORT = '3050';
+  static const String REMOTE_HOST = 'http://---';
+  static const String LOCAL_HOST = 'http://IPV4';
+  static const String PORT = '--';
 
   static const REMOTE_BASE_URL = REMOTE_HOST + ':' + PORT + API_VERSION;
   static const LOCAL_BASE_URL = LOCAL_HOST + ':' + PORT + API_VERSION;
